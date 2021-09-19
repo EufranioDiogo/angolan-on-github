f2e1c50f2343eea6d6adfd2c85b504e0abd3381b
Firmino Changani <firmino.changani@gmail.com>
2021-09-19 16:33:51 +0100

    feat: add footer component

---
+++ src/components/footer/footer.vue
@@ -0,0 +1,49 @@
+<template>
+  <footer class="footer p-4">
+    <p>
+      This project is open source under the
+      <a href="https://github.com/flowck/angolans-on-github/README.md">MIT License</a>. You can contribute by forking
+      the <a href="https://github.com/flowck/angolans-on-github">repository</a>.
+    </p>
+    <p>
+      Built with <span class="footer__heart">❤</span> by
+      <a href="https://github.com/flowck" title="Hi there, it's me">Firmino Changani</a>
+    </p>
+  </footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "Footer",
+});
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  text-align: center;
+  background-color: #24292f;
+
+  &,
+  a {
+    color: #fff;
+  }
+
+  a {
+    text-decoration: underline;
+  }
+
+  &__heart {
+    animation: infinite 1s heartBeat;
+  }
+}
+
+@keyframes heartBeat {
+  from {
+    color: red;
+  }
+  to {
+    color: pink;
+  }
+}
+</style>
